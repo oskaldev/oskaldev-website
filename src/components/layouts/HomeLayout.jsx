4d7325a5ed5ce9d688c96c325deed210c52c8c40
oskal314 <artem-osipovartem3002@mail.ru>
2023-10-05 03:26:20 +0300

    feat: added Navbar and styled Layout

--- src/components/layouts/HomeLayout.jsx
+++ src/components/layouts/HomeLayout.jsx
@@ -1,15 +1,16 @@
 import Header from 'components/common/Header';
 import Home from 'components/screens/home/Home';
+import Nav from 'components/common/Nav';
 import React from 'react';
 
 const Layout = () => {
   return (
     <>
-      <div className='py-10 bg-zinc-900 h-full'>
-        <main>
-          <Header />
-          <Home />
-        </main>
+      <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+        <Header />
+        <Nav />
+        <Home />
+        <div className='h-[4000px]'></div>
       </div>
     </>
   );
