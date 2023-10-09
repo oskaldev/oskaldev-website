31d49d8679a8a1da41a6e7cb42a0c61ff105d620
oskal314 <artem-osipovartem3002@mail.ru>
2023-10-09 22:01:47 +0300

    Feature: Updated home component Description: added a structure of 4 components for further reuse of the main component

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Banner from './banner/Banner';
+import About from './about/About';
+import Services from './services/Services';
+import Work from './work/Work';
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div> 
+      <Banner />
+      <About />
+      <Services />
+      <Work />
+    </div>
+  );
 };
 
 export default Home;
