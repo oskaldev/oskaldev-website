44c2e80048a9b6dbed67f0f86559551340501121
oskal314 <artem-osipovartem3002@mail.ru>
2023-10-12 00:14:19 +0300

    feat: added services data

---
+++ src/data/servicesData.js
@@ -0,0 +1,22 @@
+const services = [
+  {
+    name: 'Frontend Developer',
+    description:
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur labore qui totam ut excepturi iste sed, eligendi voluptatibus libero fugiat sint similique, itaque nisi aliquam tempora accusamus placeat neque consequuntur.',
+    link: 'Learn More',
+  },
+  {
+    name: 'Developer',
+    description:
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur labore qui totam ut excepturi iste sed, eligendi voluptatibus libero fugiat sint similique, itaque nisi aliquam tempora accusamus placeat neque consequuntur.',
+    link: 'Learn More',
+  },
+  {
+    name: 'Programmist',
+    description:
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur labore qui totam ut excepturi iste sed, eligendi voluptatibus libero fugiat sint similique, itaque nisi aliquam tempora accusamus placeat neque consequuntur.',
+    link: 'Learn More',
+  }
+]
+
+export default services
