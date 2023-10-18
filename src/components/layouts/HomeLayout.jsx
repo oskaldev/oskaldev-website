9cb4d386ddfc1c45c1f83c6e70ceaae02b0e29d3
oskal314 <artem-osipovartem3002@mail.ru>
2023-10-18 04:10:29 +0300

    feat: empty tags removed

--- src/components/layouts/HomeLayout.jsx
+++ src/components/layouts/HomeLayout.jsx
@@ -2,19 +2,16 @@ import Header from 'components/common/Header';
 import Home from 'components/screens/home/Home';
 import Nav from 'components/common/Nav';
 import React from 'react';
-import Contact from 'components/common/Contact'
+import Contact from 'components/common/Contact';
 
 const Layout = () => {
   return (
-    <>
-      <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
-        <Header />
-        <Nav />
-        <Home />
-        <Contact/>
-        <div className='h-[4000px]'></div>
-      </div>
-    </>
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <Header />
+      <Nav />
+      <Home />
+      <Contact />
+    </div>
   );
 };
 
